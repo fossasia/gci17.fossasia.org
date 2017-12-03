b79bde81109b04a16b971c81c7b350760cb514b5
Supun_TE <supunedirisuriya2002@gmail.com>
2017-12-04 00:17:49 +0530

    Update contributors.js

--- js/contributors.js
+++ js/contributors.js
@@ -7,7 +7,7 @@ $(function(){
     data.forEach(function(contributors){
       var html = "<div class='col-md-3 col-sm-6'>";
       html += "<a href="+contributors.html_url+">";
-      html += "<img src='icons/github.png'width='20' height='20'/>"
+      html += "<img src='https://github.com/harsit98'width='20' height='20'/>"
       html += "    "+contributors.login;
       html += "<div class='pic'></a>";
       html += "<img src="+contributors.avatar_url+"><div class='contribs'><p>";
