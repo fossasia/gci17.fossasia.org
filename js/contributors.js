b7de03b994557125e3625c1a796f5245f1e30939
Supun_TE <supunedirisuriya2002@gmail.com>
2017-12-03 14:48:46 +0530

    Added github icon.
    Added github icon in front of all contributors profile names

--- js/contributors.js
+++ js/contributors.js
@@ -10,11 +10,10 @@ $(function(){
     url: "https://api.github.com/repos/fossasia/gci17.fossasia.org/contributors"
   }).done(function(data){
     data.forEach(function(contributors){
-      
-      var html = "<div class='col-xs-12 col-sm-6 col-md-4 col-lg-3'><div class='card'>";
+      var html = "<div class='col-md-3 col-sm-6'>";
       html += "<a href="+contributors.html_url+">";
-      html += "<img src='icons/github.png' />"
-      html += contributors.login;
+      html += "<img src='icons/github.png'width='20' height='20'/>"
+      html += "    "+contributors.login;
       html += "<div class='pic'></a>";
       html += "<img src="+contributors.avatar_url+"><div class='contribs'><p>";
       html += contributors.contributions;
