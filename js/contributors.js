fb96a08c66bf225cd68934a107c94ece1381c24f
Supun_TE <supunedirisuriya2002@gmail.com>
2017-12-05 00:14:11 +0530

    Added Contributor list and reslove errors

--- js/contributors.js
+++ js/contributors.js
@@ -7,10 +7,9 @@ $(function(){
     data.forEach(function(contributors){
       var html = "<div class='col-md-3 col-sm-6'>";
       html += "<a href="+contributors.html_url+">";
-      html += "<img src='icons/github.png'width='20' height='20'/>"
-      html += "    "+contributors.login;
-      html += "<div class='pic'></a>";
-      html += "<img src="+contributors.avatar_url+"><div class='contribs'><p>";
+      html += "<img src='icons/github.png' width='18%' height='5%'/>"
+      html += "    <hover>&nbsp&nbsp"+contributors.login+"&nbsp&nbsp</hover>";
+      html += "<div id='hoverpic'><img src="+contributors.avatar_url+"></div></a>";
       html += contributors.contributions;
       
       if (contributors.contributions === 1) {
