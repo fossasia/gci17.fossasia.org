0e518f547d1c5f80ed6a4d772cc6b8e117020267
Supun_TE <supunedirisuriya2002@gmail.com>
2017-12-04 23:48:32 +0530

    Revert "Revert "Revert "Revert "Revert "Fixed"""""
    This reverts commit 333c83f62ff7578eca984c6b2f2cf911ac19d73a.

--- js/contributors.js
+++ js/contributors.js
@@ -1,15 +1,19 @@
+
+
 $(function(){
   $.ajax({
     url: "https://api.github.com/repos/fossasia/gci17.fossasia.org/contributors"
   }).done(function(data){
     data.forEach(function(contributors){
-      var html = "<div class= col-md-3 col-sm-6'>";
+      var html = "<div class='col-md-3 col-sm-6'>";
       html += "<a href="+contributors.html_url+">";
-      html += "<img src='icons/github.png'width='20%' height='20%'/>"
-      html += "    <hover>&nbsp&nbsp<font color='black'>"+contributors.login+"&nbsp&nbsp</font></hover>";
-      html += "<div id='hoverpic'><img src="+contributors.avatar_url+"></div></a>";
+      html += "<img src='icons/github.png'width='20' height='20'/>"
+      html += "    "+contributors.login;
+      html += "<div class='pic'></a>";
+      html += "<img src="+contributors.avatar_url+"><div class='contribs'><p>";
       html += contributors.contributions;
-      if (contributors.contributions == 1) {
+      
+      if (contributors.contributions === 1) {
         html += " contribution";
       }
       else {
