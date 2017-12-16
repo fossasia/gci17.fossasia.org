3eeacef52dbdff5820f30195f8acc4a8ab2f4ef2
shikhar-scs <shikhar9a@gmail.com>
2017-12-17 00:12:02 +0530

    uniform hover social media display & changed github icon

--- js/contributors.js
+++ js/contributors.js
@@ -4,7 +4,7 @@ $(function(){
     url: "https://api.github.com/repos/fossasia/gci17.fossasia.org/contributors"
   }).done(function(data){
     data.forEach(function(contributors){
-      var html = '<div class="card hvr-hang">';
+      var html = '<div class="card hvr-hang single-mentor">';
       html += '<img src="https://github.com/'+contributors.login+'.png?size=240x240" height="240" width="240">';
       html += '<br>';
       html += '<p class="person-name">'+contributors.login+' ('+contributors.contributions;
@@ -13,9 +13,9 @@ $(function(){
       } else {
         html += ' contributions)</p><br>';
       }
-      html += '<ul class="list-inline social-list">';
+      html += '<ul class="list-inline social-list social-overlay-list">';
       html += '<a href="'+contributors.html_url+'" class="icon-a">';
-      html += '<i class="ti-github icon-i"></i>';
+      html += '<i class="icon-i fa fa-github" aria-hidden="true"></i>';
       html += '</a></ul></div>';
       $("#contributors").append(html);
     });
