5464982e9403014dbe339407a6bc62734e42b8f5
Naveen Rajan <naveentherocker1234@gmail.com>
2017-12-19 18:08:01 +0530

    Added Repos in Website

--- js/contributors.js
+++ js/contributors.js
@@ -20,4 +20,26 @@ $(function(){
       $("#contributors").append(html);
     });
   });
+
+  $.ajax({
+    url: "https://api.github.com/users/fossasia/repos"
+  }).done(function(data){
+    data.forEach(function(repos){
+      var html = '<div class="card hvr-hang single-mentor">';
+      html += '<img src="https://github.com/'+repos.owner.login+'.png?size=240x240" height="240" width="240">';
+      html += '<br>';
+      html += '<p class="person-name">'+repos.name+' ('+repos.open_issues;
+      if (repos.open_issues === 1) {
+        html += ' Issue)</p><br>';
+      } else {
+        html += ' Issues)</p><br>';
+      }
+      html += '<p class="person-name">watchers: '+repos.watchers+'</p><br>';
+      html += '<ul class="list-inline social-list social-overlay-list">';
+      html += '<a href="'+repos.html_url+'" class="icon-a">';
+      html += '<i class="icon-i fa fa-github fa-2x" aria-hidden="true"></i>';
+      html += '</a></ul></div>';
+      $("#repos").append(html);
+    });
+  });
 });
