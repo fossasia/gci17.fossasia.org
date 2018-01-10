19594e631ef98610c511c5a671c568b05a45a7c2
saarthakchats <saarthakchats05@gmail.com>
2018-01-10 22:45:51 +0530

    Added Video

--- js/scripts.js
+++ js/scripts.js
@@ -1503,66 +1503,6 @@ var mr_cookies = {
     }
 };
 
-
-var TxtRotate = function(el, toRotate, period) {
-    this.toRotate = toRotate;
-    this.el = el;
-    this.loopNum = 0;
-    this.period = parseInt(period, 10) || 2000;
-    this.txt = '';
-    this.tick();
-    this.isDeleting = false;
-};
-
-TxtRotate.prototype.tick = function() {
-    var i = this.loopNum % this.toRotate.length;
-    var fullTxt = this.toRotate[i];
-
-    if (this.isDeleting) {
-        this.txt = fullTxt.substring(0, this.txt.length - 1);
-    } else {
-        this.txt = fullTxt.substring(0, this.txt.length + 1);
-    }
-
-    this.el.innerHTML = '<span class="wrap">' + this.txt + '</span>';
-
-    var that = this;
-    var delta = 300 - Math.random() * 100;
-
-    if (this.isDeleting) {
-        delta /= 2;
-    }
-
-    if (!this.isDeleting && this.txt === fullTxt) {
-        delta = this.period;
-        this.isDeleting = true;
-    } else if (this.isDeleting && this.txt === '') {
-        this.isDeleting = false;
-        this.loopNum++;
-        delta = 500;
-    }
-
-    setTimeout(function() {
-        that.tick();
-    }, delta);
-};
-
-window.onload = function() {
-    var elements = document.getElementsByClassName('txt-rotate');
-    for (var i = 0; i < elements.length; i++) {
-        var toRotate = elements[i].getAttribute('data-rotate');
-        var period = elements[i].getAttribute('data-period');
-        if (toRotate) {
-            new TxtRotate(elements[i], JSON.parse(toRotate), period);
-        }
-    }
-    // INJECT CSS
-    var css = document.createElement("style");
-    css.type = "text/css";
-    css.innerHTML = ".txt-rotate > .wrap { border-right: 0.08em solid #666 }";
-    document.body.appendChild(css);
-};
-
 // Search functions for activities.html page
 
 // can be used to implement search into other components
@@ -1612,14 +1552,14 @@ jQuery(window).scroll(function() {
     var basewrocketmeluncur = parseInt(ftrocketmeluncur.clientWidth);
     var swrocketmeluncur = scrolltoprocketmeluncur.clientWidth;
     if (basewrocketmeluncur < 1000) {
-        var leftrocketmeluncur = parseInt(fetchOffset(ftrocketmeluncur)['left']);
+        var leftrocketmeluncur = parseInt(ftrocketmeluncur.offsetLeft);
         leftrocketmeluncur = leftrocketmeluncur < swrocketmeluncur ? leftrocketmeluncur * 2 - swrocketmeluncur : leftrocketmeluncur;
-        scrolltoprocketmeluncur.style.left = (basewrocketmeluncur + leftrocketmeluncur) + 'px';
+        scrolltoprocketmeluncur.css('left', (basewrocketmeluncur + leftrocketmeluncur + "px"));
     } else {
-        scrolltoprocketmeluncur.style.left = 'auto';
-        scrolltoprocketmeluncur.style.right = '10px';
+        scrolltoprocketmeluncur.css('left', 'auto');
+        scrolltoprocketmeluncur.css('right', '10px');
     }
-})
+});
 
 jQuery('#rocketmeluncur').click(function() {
     jQuery("html, body").animate({
@@ -1634,5 +1574,5 @@ jQuery('#rocketmeluncur').click(function() {
     this.className += ' ' + "launchrocket";
     setTimeout(function() {
         self.className = 'showrocket';
-    }, 800)
+    }, 800);
 });
